5b65fc3469c26ca24ef1b1ec251dd12d75b76700
Shuhei Kanao <97086033+hono3bono3@users.noreply.github.com>
2022-08-03 09:34:08 +0900

    error

--- models/animal/error.go
+++ models/animal/error.go
@@ -8,5 +8,26 @@ type NotAnimalProblem struct {
 }
 
 func (n NotAnimalProblem) Error() string {
+	s := square{}
+	func(r rectangle) {
+
+	}(s)
 	return fmt.Sprintf("animal error! message: %s, code: %v", n.message, n.code)
+}
+
+type rectangle interface {
+	height() int
+	width() int
+}
+
+type square struct {
+	length int
+}
+
+func (sq *square) width() int {
+	return sq.length
+}
+
+func (sq *square) height() int {
+	return sq.length
 }
